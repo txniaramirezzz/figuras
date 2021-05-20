744bc24e79f87c9a11a25ff6dfa48aac5bc2661f
txnia <txnia@DESKTOP-I4GV0IQ>
2021-05-20 12:50:36 -0600

    Figuras clase

---
+++ src/figuras/Figura.java
@@ -0,0 +1,17 @@
+
+package figuras;
+
+public abstract class Figura {
+    Figura(){
+        
+    }
+    public double calcularperimetro(){
+        return 0;
+    }
+    
+   public double calculararea(){
+       return 0;
+   } 
+    
+    
+}
